4e855a8d42e9bce741734da458be26015bbf5a4a
Matvey Tikhomirov <matvey@debian.debian>
2025-01-13 17:29:42 -0500

    empty message

---
+++ Treap.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include<vector>
+#define int long long
+
+using namespace std;
+
+struct Node{
+    int x;
+    int y;
+    Node* left;
+    Node* right;
+};
+
+struct Treap{
+    void merge(){
+        
+    }
+}
+
+signed main(){
+    
+}
